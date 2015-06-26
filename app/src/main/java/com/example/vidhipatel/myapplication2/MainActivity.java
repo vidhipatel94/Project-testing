09b5e6514827f0bd1086acabb4525e7bf6e613a4
vidhipatel94 <vidhipatel94@gmail.com>
2015-06-26 12:25:56 +0530

    Toast changed

--- app/src/main/java/com/example/vidhipatel/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/vidhipatel/myapplication2/MainActivity.java
@@ -13,7 +13,8 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(getApplicationContext(),"x",Toast.LENGTH_LONG).show();
+	//msg changed
+        Toast.makeText(getApplicationContext(),"hello",Toast.LENGTH_LONG).show();
     }
 
     @Override
